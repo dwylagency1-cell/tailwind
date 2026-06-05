e16d23683cc0539270d248edcc9a6536f9c820b5
Dekiru <dwyl.agency1@gmail.com>
2026-06-05 22:49:44 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Page1 from './components/page1/page1'
+import Page2 from './components/page2/page2'
 function App() {
-  return <div>
-    <h1>App</h1>
+  return <div >
+    <Page1/>
+    <Page2/>
+    
   </div>
 }
 
