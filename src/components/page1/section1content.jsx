187c7b8ba4058b2585f43247f8529cb57a7e7ade
Dekiru <dwyl.agency1@gmail.com>
2026-06-05 22:55:26 +0530

    section1content.jsx

---
+++ src/components/page1/section1content.jsx
@@ -0,0 +1,10 @@
+import LeftContent from './leftText'
+import ImageContent from './imageContainer'
+function Section1() {
+    return <div className="bg-blue-400 h-[90vh] flex items-center justify-between ">
+        <LeftContent/>
+        <ImageContent/>
+        
+    </div>
+}
+export default Section1
