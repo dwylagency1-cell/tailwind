8b6e1e79f4467e0afbe8f003e5e6b45e8111b435
Dekiru <dwyl.agency1@gmail.com>
2026-06-05 22:52:40 +0530

    navbar.jsx

---
+++ src/components/page1/navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+    return <div className="bg-gray-900 h-[80px] flex items-center justify-between" >
+        
+        <div className=" w-[300px] h-[80px] flex items-center justify-center">
+            <div className="bg-black  z-100 w-[150px] pt-1.5 pb-1.5 flex items-center justify-center rounded-[20px]">
+                <p className="para uppercase">Target Audience</p>
+            </div>
+        </div>
+        <div className=" w-[300px] h-[80px] flex items-center justify-center">
+            <div className="bg-white  z-100 w-[250px] pt-1.5 pb-1.5 flex items-center justify-center rounded-[20px]">
+                <p className="para2 uppercase">Digital Banking Platform</p>
+            </div>
+        </div>
+    </div>
+}
+export default Navbar
