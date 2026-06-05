2e49d136400e9cd0113ceb3bf190ee5d97120ee5
Dekiru <dwyl.agency1@gmail.com>
2026-06-06 00:17:30 +0530

    leftText.jsx

--- src/components/page1/leftText.jsx
+++ src/components/page1/leftText.jsx
@@ -1,12 +1,15 @@
+import {ArrowUpRight} from 'lucide-react'
 function LeftContent() {
     return <div>
-        <div className="bg-red-400 w-[300px] h-[550px] ml-[70px]">
-            <div className="bg-gray-400 w-[300px] h-[400px]">
+        <div className=" w-[300px] h-[550px] ml-[70px]">
+            <div className=" w-[300px] h-[400px]">
                 <h1 className="text-5xl pt-[40px] font-semibold leading-[50px]">Prospective customer segmentation</h1>
                 <p className="mt-[30px]">Depending on customer satisfactionand access to banking products, potentia target audience can be divided into three groups</p>
 
             </div>
-            <div className="bg-yellow-300 w-[300px] h-[150px]"></div>
+            <div className=" w-[300px] h-[150px] flex items-center justify-left">
+                <ArrowUpRight size={50} />
+            </div>
         </div>
     </div>
 }
