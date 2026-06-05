a32be863f11c27e282a4cbcc37a28015a7e1d17e
Dekiru <dwyl.agency1@gmail.com>
2026-06-06 00:18:42 +0530

    imageContainer.jsx

--- src/components/page1/imageContainer.jsx
+++ src/components/page1/imageContainer.jsx
@@ -1,6 +1,10 @@
+import RightCard from './rightCard'
 function ImageContent() {
     return <div>
-        <div className="bg-red-400 w-[900px] h-[550px] mr-[70px]"></div>
+        <div className="bg-red-400 w-[900px] h-[550px] mr-[70px] p-6">
+            <RightCard/>
+
+        </div>
     </div>
 }
 export default ImageContent
