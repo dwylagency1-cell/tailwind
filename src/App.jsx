eeec4ba87d97d2123e8f46e41f153e599a4fd1f1
Dekiru <dwyl.agency1@gmail.com>
2026-06-06 23:40:58 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import Page1 from './components/page1/page1'
 import Page2 from './components/page2/page2'
+import professional from './components/image/professional.jpg'
+import professional_2 from './components/image/professional_2.jpg'
+import professional_3 from './components/image/professional_3.jpg'
 function App() {
+  const user = [
+    {
+      img: professional,
+      intro: "",
+      tag: "Stisfied"
+    },
+    {
+      img: professional_2,
+      intro: "",
+      tag: "Underserver"
+    },
+    {
+      img: professional_3,
+      imtro: "",
+      tag: "  Underbanked"
+    }
+  ]
+
   return <div >
-    <Page1/>
+    <Page1 users = {user}/>
     <Page2/>
     
   </div>
