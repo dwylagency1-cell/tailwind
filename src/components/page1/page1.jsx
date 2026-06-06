422753564e560002ca5f6bf9e605371f5bc23afe
Dekiru <dwyl.agency1@gmail.com>
2026-06-06 23:42:16 +0530

    page1.jsx

--- src/components/page1/page1.jsx
+++ src/components/page1/page1.jsx
@@ -1,9 +1,9 @@
 import Navbar from './navbar'
 import Section1 from './section1content'
-function Page1() {
+function Page1(props) {
     return <div className="h-screen w-full bg-white-900">
         <Navbar/>
-        <Section1/>
+        <Section1 user = {props.users}/>
 
     </div>
 }
