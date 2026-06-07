0727031c5b5b329930797ea22a0c03146d691fd9
Dekiru <dwyl.agency1@gmail.com>
2026-06-07 15:43:33 +0530

    imageContainer.jsx

--- src/components/page1/imageContainer.jsx
+++ src/components/page1/imageContainer.jsx
@@ -1,10 +1,10 @@
 import RightCard from './rightCard'
-function ImageContent() {
+function ImageContent(props) {
     return <div>
-        <div className="bg-red-400 w-[900px] h-[550px] mr-[70px] p-6 flex item-center gap-x-[20px]">
-            <RightCard/>
-            <RightCard/>
-            <RightCard/>
+        <div className=" w-[900px] h-[550px] mr-[70px] p-6 flex item-center gap-x-[20px]">
+            {props.users.map(function(elem) {
+                return <RightCard img = {elem.img} intro = {elem.intro} tag = {elem.tag}/>
+            }) }
 
         </div>
     </div>
